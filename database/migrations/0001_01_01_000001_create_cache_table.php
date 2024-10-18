8804c516d8747624f566fcf1bcf036f14c7b1f6b
rosarisimanjuntak16 <112604185+rosarisimanjuntak16@users.noreply.github.com>
2024-10-18 16:46:43 +0700

    mencoba database

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('owner');
             $table->integer('expiration');
         });
-    }
+    } 
 
     /**
      * Reverse the migrations.
